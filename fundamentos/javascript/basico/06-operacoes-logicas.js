02e9ae507017087ac50491dd8641e38132690c49
maikon <maikon@gmail.com>
2020-07-31 06:48:00 -0400

    Javascript - 08 - Operações Lógicas (Parte 2)

--- fundamentos/javascript/basico/06-operacoes-logicas.js
+++ fundamentos/javascript/basico/06-operacoes-logicas.js
@@ -30,3 +30,37 @@ console.log(a);
 a = 1 === "1";
 console.log(a);
 
+// Expressoes logicas
+
+var e = [];
+var x = 10, y = 20, z = 30;
+
+e[0] = x == 10 || y == 100; // (Expr.1) OU (Expr.2)
+e[1] = x != 10 && y == 100; // (Expr.1) E  (Expr.2)
+e[2] = x == 10 && y == 100; // (Expr.1) E  (Expr.2)
+e[3] = x == 10 && y == 20;  // (Expr.1) E  (Expr.2)
+e[4] = ((x == "10" || y == 99) && y == 20);  // ((Expr.1 OU Expr.2) E  (Expr.3))
+console.log(e);
+
+
+// Operador ternario
+
+a = (x == 10) ? 100 : 200;
+b = (x == 20) ? 100 : 200;
+console.log("a = " + a + " b = " + b);
+
+
+// Se.... entao faca isso, caso contrario, faca aquilo
+if (b) 
+    console.log("Valor de b = verdadeiro");
+else
+    console.log("Valor de b = falso");
+
+if (b) {
+    console.log("Comando 1: Valor de b = verdadeiro");
+    console.log("Comando 2: Valor de b = verdadeiro");
+}
+else
+    console.log("Valor de b = falso");
+
+
