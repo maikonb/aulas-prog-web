f4f916ec46106db97be5ab3dc0c48991f1563f4d
maikon <maikon@gmail.com>
2020-07-31 10:08:36 -0400

    Javascript no Navegador - 09 - Envio dados de formulario - Parte 2

--- fundamentos/javascript/vanilla-js/ex5-server.js
+++ fundamentos/javascript/vanilla-js/ex5-server.js
@@ -2,7 +2,19 @@ const http = require('http');
 const port = 3000;
 
 const requestHandler = (req, res) => {
-  console.log('requestHandler()');
+
+  console.log('URL: ', req.url);
+  console.log('Method: ', req.method);
+
+  if (req.url==='/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Oi</h1>');
+  }
+  else {
+    res.writeHead(404);
+    res.end();
+  }
+
 };
 
 const server = http.createServer( requestHandler );
