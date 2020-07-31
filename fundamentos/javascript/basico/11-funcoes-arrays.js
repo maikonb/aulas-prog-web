4d82d80c0b01bbfc22ba4dad7fc0ec96acc8b786
maikon <maikon@gmail.com>
2020-07-31 07:10:24 -0400

    Javascript - 17 - Arrays e seus métodos - Parte 2

--- fundamentos/javascript/basico/11-funcoes-arrays.js
+++ fundamentos/javascript/basico/11-funcoes-arrays.js
@@ -11,3 +11,17 @@ nomes.forEach( (nome, i) => {
   console.log(`nome[${i}] = ${nome}`);
 });
 
+//
+console.log('\nindexOf =>> ');
+console.log('indexof(rosangela) = ', nomes.indexOf('rosangela') );
+
+//
+console.log('\nsplice =>> ');
+var v = Array.from(nomes); // copia do array
+console.log('antes: ', v);
+v.splice(3, 2);
+console.log('removendo 2, a partir da pos 3: ', v);
+v.splice(1, 1);
+console.log('removendo 1, a partir da pos 1: ', v);
+
+//
