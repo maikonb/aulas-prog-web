f11aeee80a14fbfaf3706990e630a3e1b16f90d0
maikon <maikon@gmail.com>
2020-07-31 06:54:06 -0400

    Javascript - 13 - Funções (Parte 2)

--- fundamentos/javascript/basico/09-funcoes.js
+++ fundamentos/javascript/basico/09-funcoes.js
@@ -16,3 +16,47 @@ imprimirObjeto( {
   idade: 40
 });
 
+// Primitive parameters (such as a number) are passed to functions by value; 
+// the value is passed to the function, but if the function changes the value 
+// of the parameter, this change is not reflected globally or in the calling 
+// function.
+
+// If you pass an object (i.e. a non-primitive value, such as Array or a 
+// user-defined object) as a parameter and the function changes the object's 
+// properties, that change is visible outside the function
+
+function modificarTipoPrimitivo(n) {
+  n=5;
+}
+function modificarObjeto(o) {
+  if ( o != null && typeof o == "object") {
+    o.email = "jose@miranda.com";
+    if (o.hasOwnProperty('idade'))
+      o.idade = 52;
+  }
+}
+function modificarArray(a) {
+  if (Array.isArray(a))
+    a.push(1000,2000,3000);
+}
+
+var n = 100;
+o = {
+  id: 1,
+  nome: "Jose Miranda",
+  idade: 40
+};
+a = [10,20,30];
+
+console.log('tipo primitivo antes : ', n);
+modificarTipoPrimitivo(n);
+console.log('tipo primitivo depois: ', n);
+
+console.log('tipo objeto antes : ', o);
+modificarObjeto(o);
+console.log('tipo objeto depois: ', o);
+
+console.log('tipo array antes : ', a);
+modificarArray(a);
+console.log('tipo array depois: ', a);
+
