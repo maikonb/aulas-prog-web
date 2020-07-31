e37812bff22cf313f78d18cd9546fdc41713b304
maikon <maikon@gmail.com>
2020-07-31 06:42:12 -0400

    Javascript - 05 - Arrays

---
+++ fundamentos/javascript/basico/03-arrays.js
@@ -0,0 +1,29 @@
+
+var computador = ["memoria", "disco", "placa mae", "processador"];
+console.log(computador);
+console.log(computador[0]);
+console.log(computador[1]);
+console.log(computador[2]);
+console.log(computador[3]);
+
+var pratos = ["feijoada", "arroz", , "farofa", "bife"];
+console.log(pratos);
+console.log(pratos[2]);
+
+var diversos = ["uma string", 100, true, 0.99, null];
+console.log(diversos);
+
+var v = [];
+v.push(10);
+v.push(20);
+v.push(30);
+v.push(40);
+v.push(50);
+console.log(v);
+
+
+console.log('pop', v.pop(), v);
+console.log('pop', v.pop(), v);
+console.log('pop', v.pop(), v);
+console.log('pop', v.pop(), v);
+console.log('pop', v.pop(), v);
