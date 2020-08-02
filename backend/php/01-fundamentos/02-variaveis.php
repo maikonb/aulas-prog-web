6dd59ae025d77398ed34bcb1a93e254873acd55f
maikon <maikon@gmail.com>
2020-08-02 07:40:29 -0400

    PHP - Fundamentos - 03 Variáveis (Parte 2)

--- backend/php/01-fundamentos/02-variaveis.php
+++ backend/php/01-fundamentos/02-variaveis.php
@@ -22,3 +22,13 @@
   printf("<p> varFloat = %08.4f  </p>", $varFloat);
   printf("<p> varInt = %08d  </p>", $varInt);
 
+  if (is_string($varString)) 
+    echo "<h2>varString é uma String</h2>";
+  if (is_bool($varBool)) 
+    echo "<h2>varBool é um Bool</h2>";    
+  if (is_int($varInt)) 
+    echo "<h2>varInt é um Int</h2>"; 
+  if (is_float($varFloat)) 
+    echo "<h2>varFloat é um Float</h2>"; 
+
+    
