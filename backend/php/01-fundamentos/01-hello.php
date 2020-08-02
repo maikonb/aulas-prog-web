27dedbdf2a9960d382a74d5d4650b1f160738f8d
maikon <maikon@gmail.com>
2020-08-02 07:12:15 -0400

    PHP - Fundamentos - 01 Hello World

---
+++ backend/php/01-fundamentos/01-hello.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World - PHP</title>
+</head>
+<body>
+
+<?php
+  echo "<h1>Hello World from PHP 1</h1>";
+?>
+
+<h1>Hello World from HTML</h1>
+
+<?php
+  echo "<h1>Hello World from PHP 2</h1>";
+?>
+
+</body>
+</html>
+
