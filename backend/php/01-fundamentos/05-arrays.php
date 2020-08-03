3cefc945347d1ce7dda884a1a2f68dde34821a47
maikon <maikon@gmail.com>
2020-08-03 06:26:12 -0400

    PHP - Fundamentos - 08 Arrays (Parte 2)

--- backend/php/01-fundamentos/05-arrays.php
+++ backend/php/01-fundamentos/05-arrays.php
@@ -17,4 +17,22 @@
   $a[] = false;
   $a[] = [10,20,30,40,50];
   var_dump($a);
+  echo("<p>Na posicao 5: $a[5]</p>");
 
+  echo("<br><hr>");
+
+  echo("<h2>Arrays Associativos</h2>");
+  $b['a'] = 10;
+  $b['b'] = 20;
+  $b['c'] = 30;
+  $b['d'] = 40;
+  $b['e'] = 'Letra E';
+  $b['letra f'] = 400.88888;
+  $b["array a"] = [1,2,3,4];
+  $b[5] = 'numero 5';
+  var_dump($b);
+  echo("<p>Na posicao 5: $b[5]</p>");
+  echo("<p>Na posicao 'b': " . $b['b'] ."</p>");
+  echo("<p>Na posicao 'letra f': " . $b['f'] ."</p>");
+
+  
