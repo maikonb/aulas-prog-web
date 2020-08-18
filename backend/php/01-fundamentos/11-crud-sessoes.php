90b24c81902482c472b40af4fdc25698648ec7f0
maikon <maikon@gmail.com>
2020-08-18 07:10:07 -0400

    PHP - Fundamentos - CRUD em Sessao (Parte 2)

--- backend/php/01-fundamentos/11-crud-sessoes.php
+++ backend/php/01-fundamentos/11-crud-sessoes.php
@@ -1,3 +1,67 @@
+<?php
+  session_start();
+  $pessoas =  isset($_SESSION['pessoas']) ? 
+                isset($_SESSION['pessoas']) : [];
+
+  function gerarNovoId()
+  {
+    if (isset($_SESSION['ultimoID']))
+      ++$_SESSION['ultimoID'];
+    else
+      $_SESSION['ultimoID'] = 1;
+    return $_SESSION['ultimoID'];
+  }
+
+  function cadastrarPessoa($dadosPessoa) 
+  {
+    global $pessoas;
+    $id = gerarNovoId();
+    $pessoas[ $id ] = $dadosPessoa;
+  } 
+
+  function empacotaDadosForm() 
+  {
+    $dados = [];
+    $dados['nome'] = $_POST['nome'];
+    $dados['idade'] = $_POST['idade'];
+    $dados['telefone'] = $_POST['telefone'];
+    $dados['cidade'] = $_POST['cidade'];
+    if (isset($_POST['id']) && $_POST['id'] != '')
+      $dados['id'] = $_POST['id'];
+    return $dados;
+  }
+
+  function validaDadosForm()
+  {
+    return (
+      (isset($_POST['nome']) && $_POST['nome'] != "") && 
+      (isset($_POST['idade']) && $_POST['idade'] != "") && 
+      (isset($_POST['telefone']) && $_POST['telefone'] != "") && 
+      (isset($_POST['cidade']) && $_POST['cidade'] != ""));
+  }
+
+  function processarAcao($acao) 
+  {
+    switch($acao) {
+      case 'Cadastrar':
+        if (validaDadosForm()) {
+          $dadosPessoa = empacotaDadosForm();
+          cadastrarPessoa($dadosPessoa);
+        }
+        break;      
+      case 'Atualizar':
+        break;
+      case 'Apagar':
+        break;
+      case 'Cancelar':
+        break;
+    }
+  }
+
+  if (isset($_POST['acao']) && $_POST['acao'] != '')
+    processarAcao($_POST['acao']);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,7 +100,8 @@
       </tr>
       <tr>
         <td>
-          <input type="submit" value="Cadastrar">
+          <input type="submit" value="Cadastrar" name="acao">
+          <input type="submit" value="Atualizar" name="acao">
           <input type="reset" value="Cancelar">
         </td>
       </tr>      
