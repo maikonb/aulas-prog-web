1df4c2cd121b9002354fa633bad286ab485011eb
maikon <maikon@gmail.com>
2020-08-18 07:08:30 -0400

    PHP - Fundamentos - CRUD em Sessao (Parte 1)

---
+++ backend/php/01-fundamentos/11-crud-sessoes.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro de Pessoas</title>
+</head>
+<body>
+  <h2>Cadastro de Pessoas</h2>
+  <form action="">
+    <input type="hidden" name="id" value="">
+    <table>
+      <tr>
+        <td>Nome</td>
+        <td>
+          <input type="text" name="nome" value="">
+        </td>
+      </tr>
+      <tr>
+        <td>Idade</td>
+        <td>
+          <input type="number" name="idade" value="">
+        </td>
+      </tr>      
+      <tr>
+        <td>Telefone</td>
+        <td>
+          <input type="text" name="telefone" value="">
+        </td>
+      </tr>
+      <tr>
+        <td>Cidade</td>
+        <td>
+          <input type="text" name="cidade" value="">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <input type="submit" value="Cadastrar">
+          <input type="reset" value="Cancelar">
+        </td>
+      </tr>      
+    </table>
+  </form>
+</body>
+</html>
