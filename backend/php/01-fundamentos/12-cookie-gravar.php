5a139e8baf4827e6ad3cf8f51351be6d0bdd188d
maikon <maikon@gmail.com>
2020-08-19 10:14:18 -0400

    PHP - Fundamentos - Cookies

---
+++ backend/php/01-fundamentos/12-cookie-gravar.php
@@ -0,0 +1,7 @@
+<?php 
+  setcookie('nome', 'marcelo');
+  setcookie('sobrenome', 'silva');
+  setcookie('cidade', 'rondonopolis');
+  setcookie('estado', 'mato grosso');
+  setcookie('universidade', 'ufr', time() + 10);
+?>
