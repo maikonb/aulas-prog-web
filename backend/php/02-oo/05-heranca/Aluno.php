c08ac9baf6407272b1d424f18e74c43bc7eb9aac
maikon <maikon@gmail.com>
2020-08-21 07:11:45 -0400

    PHP - Orientação a Objetos - Herança (parte 2)

--- backend/php/02-oo/05-heranca/Aluno.php
+++ backend/php/02-oo/05-heranca/Aluno.php
@@ -4,3 +4,33 @@
 // require('Pessoa.php');
 // include_once('Pessoa.php');
 require_once('Pessoa.php');
+
+class Aluno extends Pessoa {
+
+  private $linguagens;
+  private $matricula;
+
+  public function __construct($nome='', $idade=0, $matricula) 
+  {
+    parent::__construct($nome, $idade);
+    $this->matricula = $matricula;
+    $this->linguagens = [] ;
+  }
+
+  public function addLinguagem($linguagem) {
+    $this->linguagens[] = $linguagem;
+  }
+
+  public function getLinguagens() {
+    return $this->linguagens;
+  }
+
+  public function setMatricula($matricula) {
+    $this->matricula = $matricula; 
+  }
+
+  public function getMatricula() {
+    return $this->matricula; 
+  }
+
+}
