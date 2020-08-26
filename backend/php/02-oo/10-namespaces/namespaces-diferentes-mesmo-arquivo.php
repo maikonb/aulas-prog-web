abd2a6c951f76826d33f899f6a31cd91b5ad5066
maikon <maikon@gmail.com>
2020-08-26 05:16:02 -0400

    PHP - Orientação a Objetos - Namespaces - Hierarquias

--- backend/php/02-oo/10-namespaces/namespaces-diferentes-mesmo-arquivo.php
+++ backend/php/02-oo/10-namespaces/namespaces-diferentes-mesmo-arquivo.php
@@ -24,3 +24,31 @@ namespace {
   $c = new ClasseTeste1();
   echo $c;
 }
+
+/////////////////////////////////////////////
+
+namespace Classes\NameSpaceX {
+  class ClasseTeste2 {
+    public function __toString()
+    {
+      return "<h1>Sou a ClasseTeste2 do Classes\NameSpaceX</h1>" . 
+        "<p>" . __NAMESPACE__ . "</p>";
+    }
+  }
+}
+
+namespace Classes\NameSpaceY {
+  class ClasseTeste2 {
+    public function __toString()
+    {
+      return "<h1>Sou a ClasseTeste2 do Classes\NameSpaceY</h1>" . 
+        "<p>" . __NAMESPACE__ . "</p>";
+    }
+  }
+}
+
+namespace {
+  use Classes\NameSpaceY\ClasseTeste2;
+  $c1 = new ClasseTeste2();
+  echo $c1;
+}
