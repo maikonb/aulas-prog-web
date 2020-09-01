88ba6150c1b3517828564cb1e2e19c2aeeb9f017
maikon <maikon@gmail.com>
2020-09-01 07:54:33 -0400

    PHP - MySQL - Conexão com o banco de dados

---
+++ backend/php/03-bd/01-conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+$host   = "127.0.0.1";
+$user   = "user";
+$passwd = "user";
+$db     = "vendas";
+
+$mysqli = new mysqli($host, $user, $passwd, $db);
+$conectado = false;
+
+if (!$mysqli->connect_errno) {
+  echo "<p>Conectado ao banco de dados! </p>\n";
+  $conectado = true;
+}
+else
+  echo "<p>Não conectado ao banco de dados. Erro na conexão. </p>\n";
+
+
