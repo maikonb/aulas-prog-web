aac54ece5f35511e7db8c85892412c2b7aeff432
maikon <maikon@gmail.com>
2020-09-22 07:35:26 -0400

    Laravel - DB - Migrations - Entendendo como o migration funciona e criando a tabela 'marcas'

---
+++ frameworks/laravel/03-db-migrations-e-modelos/database/migrations/2020_09_22_110444_create_table_marcas.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableMarcas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marcas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marcas');
+    }
+}
