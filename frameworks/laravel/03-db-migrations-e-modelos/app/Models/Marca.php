a17b55dfa4efced1b8b1f2afd6f42d7d4b480755
maikon <maikon@gmail.com>
2020-09-24 06:02:50 -0400

    Laravel - DB - ORM - Nomes das tabelas

--- frameworks/laravel/03-db-migrations-e-modelos/app/Models/Marca.php
+++ frameworks/laravel/03-db-migrations-e-modelos/app/Models/Marca.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     use HasFactory;
+
+    // Se o nome de seu model eh Marca, o laravel
+    // infere que o nome da tabela, ligada a esse 
+    // model, se chama 'marcaS', no plural. Essa eh
+    // uma convencao de nomenclatura do Laravel
+    // que visa facilitar a utilizacao do ORM.
+    // Porem, se sua tabela nao se chamar 'marcaS', 
+    // voce pode utilizar o atributo abaixo para
+    // indicar ao Laravel qual o nome correto da 
+    // tabela.
+    protected $table = 'marcas';
 }
