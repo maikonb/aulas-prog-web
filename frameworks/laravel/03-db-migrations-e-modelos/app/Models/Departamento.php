2be071cc26a85cfd8d4df77d80005031ae71dd7e
maikon <maikon@gmail.com>
2020-10-13 07:32:01 -0400

    Laravel - DB - ORM - Relacionamento muitos para Muitos - parte 2

--- frameworks/laravel/03-db-migrations-e-modelos/app/Models/Departamento.php
+++ frameworks/laravel/03-db-migrations-e-modelos/app/Models/Departamento.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Departamento extends Model
 {
     use HasFactory;
+
+    public function produtos()
+    {
+        return $this->belongsToMany(
+            'App\Models\Produto', 'produto_departamento');
+    }
 }
